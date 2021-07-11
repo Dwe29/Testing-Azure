5a557159b795ab90d17970cdbbc5eb84422f4048
Dwe29 <wahyu5161.w5@gmail.com>
2021-07-11 15:13:15 +0800

    testing 4

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
-<?php
-    echo "Nama: Wahyu Dwi Hartarto";
-    echo "NIM: 20.11.3356";
-    echo "Mata Kuliah: Sistem Operasi";
-    echo "UNIVERSITAS AMIKOM";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <h1>Nama: Wahyu Dwi Hartarto</h1>
+    <br>
+    <h1>NIM: 20.11.3356</h1>
+    <br>
+    <h1>Mata Kuliah: Sistem Operasi</h1>
+    <br>
+    <h1>UNIVERSITAS AMIKOM</h1>
+</head>
+<body>
+    
+</body>
+</html>
